b2613ed2bdc5f4b75ae0ad4a9631007f0f4bf0d1
petrovale <leha.isakov@yandex.ru>
2019-08-13 23:35:26 +0300

    Added Owners Lost and updated controller

--- pet-clinic-web/src/main/java/ru/org/spring/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/ru/org/spring/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package ru.org.spring.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.org.spring.sfgpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+  private final OwnerService ownerService;
+
+  public OwnerController(OwnerService ownerService) {
+    this.ownerService = ownerService;
+  }
+
   @RequestMapping({"", "/", "/index", "/index.html"})
-  public String listOwners(){
+  public String listOwners(Model model) {
+
+    model.addAttribute("owners", ownerService.findAll());
     return "owners/index";
   }
 }
