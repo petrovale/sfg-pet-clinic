db861facb3e273da5dcff508aac3420e185379ab
petrovale <leha.isakov@yandex.ru>
2019-08-13 23:52:00 +0300

    Added Vet List and updated controller

--- pet-clinic-web/src/main/java/ru/org/spring/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/ru/org/spring/sfgpetclinic/controllers/VetController.java
@@ -3,13 +3,21 @@ package ru.org.spring.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.org.spring.sfgpetclinic.services.VetService;
 
 @Controller
 public class VetController {
 
+  private final VetService vetService;
+
+  public VetController(VetService vetService) {
+    this.vetService = vetService;
+  }
+
   @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
   public String listVets(Model model) {
 
+    model.addAttribute("vets", vetService.findAll());
     return "vets/index";
   }
 }
