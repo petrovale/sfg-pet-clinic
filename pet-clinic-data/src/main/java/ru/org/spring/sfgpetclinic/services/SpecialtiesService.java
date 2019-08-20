9b15b90db59e58ffbf1e9d6e890d56d755a2e4b5
petrovale <leha.isakov@yandex.ru>
2019-08-21 00:21:35 +0300

    Added Speciality Map impl

---
+++ pet-clinic-data/src/main/java/ru/org/spring/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package ru.org.spring.sfgpetclinic.services;
+
+import ru.org.spring.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long>  {
+
+}
---
+++ pet-clinic-data/src/main/java/ru/org/spring/sfgpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,36 @@
+package ru.org.spring.sfgpetclinic.services.map;
+
+import java.util.Set;
+import org.springframework.stereotype.Service;
+import ru.org.spring.sfgpetclinic.model.Speciality;
+import ru.org.spring.sfgpetclinic.services.SpecialtiesService;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements
+    SpecialtiesService {
+
+  @Override
+  public Set<Speciality> findAll() {
+    return super.findAll();
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    super.deleteById(id);
+  }
+
+  @Override
+  public void delete(Speciality object) {
+    super.delete(object);
+  }
+
+  @Override
+  public Speciality save(Speciality object) {
+    return super.save(object);
+  }
+
+  @Override
+  public Speciality findById(Long id) {
+    return super.findById(id);
+  }
+}
