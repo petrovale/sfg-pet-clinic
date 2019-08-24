f58634ac5984688ddcd7ab74ac5f40e3bf5f332c
petrovale <leha.isakov@yandex.ru>
2019-08-24 16:43:45 +0300

    added Project Lombok

--- pet-clinic-data/src/main/java/ru/org/spring/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/ru/org/spring/sfgpetclinic/model/Person.java
@@ -2,7 +2,11 @@ package ru.org.spring.sfgpetclinic.model;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @MappedSuperclass
 public class Person extends BaseEntity {
 
@@ -10,20 +14,4 @@ public class Person extends BaseEntity {
   private String firstName;
   @Column(name = "last_name")
   private String lastName;
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
 }
--- pet-clinic-data/src/main/java/ru/org/spring/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/ru/org/spring/sfgpetclinic/model/Vet.java
@@ -8,7 +8,17 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -16,12 +26,4 @@ public class Vet extends Person {
   @ManyToMany(fetch = FetchType.EAGER)
   @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
   private Set<Speciality> specialities = new HashSet<>();
-
-  public Set<Speciality> getSpecialities() {
-    return specialities;
-  }
-
-  public void setSpecialities(Set<Speciality> specialities) {
-    this.specialities = specialities;
-  }
 }
