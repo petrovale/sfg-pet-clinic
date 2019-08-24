7e60065eaaa4e5d3b6af360e6d1e24c92a75b2b5
petrovale <leha.isakov@yandex.ru>
2019-08-24 12:49:01 +0300

    added Pet SD Jpa Service

--- pet-clinic-data/src/main/java/ru/org/spring/sfgpetclinic/repositories/PetRepository.java
+++ pet-clinic-data/src/main/java/ru/org/spring/sfgpetclinic/repositories/PetRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.repository.CrudRepository;
 import ru.org.spring.sfgpetclinic.model.Pet;
 
 public interface PetRepository extends CrudRepository<Pet, Long> {
-
 }
--- pet-clinic-data/src/main/java/ru/org/spring/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/ru/org/spring/sfgpetclinic/services/PetService.java
@@ -3,6 +3,4 @@ package ru.org.spring.sfgpetclinic.services;
 import ru.org.spring.sfgpetclinic.model.Pet;
 
 public interface PetService extends CrudService<Pet, Long> {
-
-  Pet findByLastName(String lastName);
 }
